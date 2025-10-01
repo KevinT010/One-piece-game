caa6a82ca9253c35dbcd944a3697570008477c9b
KEVN <troostkevin8@gmail.com>
2025-10-01 16:32:15 +0200

    added female characters

--- Code/main.py
+++ Code/main.py
@@ -12,7 +12,61 @@ def yes_or_no(question):
 def guess_character():
     input(" Welcome to one piece akinitor you can choose between those characters: Franky, Brook, Luffy, Zoro, Sanji, Chopper, Jinbei, Shanks, King cobra, Blackbeard, \n Nami, Robin, Al vida, Vivi, Shiraoshi, Rebecca, Reiju, Big mom, Shaky, Boa hancock \n Press Enter to continue...")
     if yes_or_no("Is this character a female? "):
-        print("You selected a female character.")
+        if yes_or_no("Is this character a straw hat pirate?"):
+            if yes_or_no("Is this character a navigator?"):
+                if yes_or_no("Is your character Nami?"):
+                    print("Your character is Nami")
+                else:
+                    print("The character is not in the list")
+            else:
+                  if yes_or_no("Is your character Robin?"):
+                     print("Your character is Robin")
+                  else:
+                      print("The character is not in the list")
+        else:
+            if yes_or_no("Is this character currently a pirate?"):
+                if yes_or_no("is/was this character a yonko?"):
+                    if yes_or_no("Is your character Big mom?"):
+                        print("Your character is Big mom")
+                    else:
+                        print("The character is not in the list")
+                else:
+                    if yes_or_no("Is she a empress?"):
+                        if yes_or_no("Is your character Boa hancock?"):
+                            print("Your character is Boa hancock")
+                        else:
+                            print("The character is not in the list")
+                    else:
+                        if yes_or_no("is your character Alivda?"):
+                          print("Your character is Alvida")
+                        else:
+                          print("The character is not in the list")
+            else:
+                if yes_or_no("Is this character a princess?"):
+                    if yes_or_no("has your character pink hair?"):
+                        if yes_or_no("Is your character Vivi?"):
+                            if yes_or_no("is your character a fishman?"):
+                                if yes_or_no("Is your character Shirahoshi?"):
+                                    print("Your character is Shirahoshi")
+                                else:
+                                    print("The character is not in the list")
+                            else:
+                                if yes_or_no("Is your character Reiju?"):
+                                    print("Your character is Reiju")
+                                else:
+                                    print("The character is not in the list")             
+                else:
+                    if yes_or_no("Is she a gladiator?"):
+                        if yes_or_no("Is your character Rebecca?"):
+                            print("Your character is Rebecca")
+                        else:
+                            print("The character is not in the list")
+                    else:
+                        if yes_or_no("Is your character Shakky?"):
+                            print("Your character is Shakky")
+                        else:
+                            print("The character is not in the list")
+                  
     else:
         if yes_or_no("Is this character a straw hat pirate?"):
            if yes_or_no("Is this character the captain?"):
